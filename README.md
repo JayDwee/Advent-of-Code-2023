5cb8bf21ff916c82737613b2ddff89d9ff49e3ab
Jack Draper <drapj002@gmail.com>
2023-12-07 21:59:29 +0000

    Day 7: Camel Cards

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # [🎄 Advent of Code 2023](https://adventofcode.com/2023)
-![Static Badge](https://img.shields.io/badge/⭐_Stars-6_of_50-gold)
+![Static Badge](https://img.shields.io/badge/⭐_Stars-14_of_50-gold)
 
 <img src="https://theminimalistvegan.com/wp-content/uploads/2017/12/pexels-photo-704219.jpeg" align="right" height="156" width="128" style="object-fit:cover;object-position:-18px" />
 
@@ -8,13 +8,13 @@ Advent of Code is an Advent calendar of small programming puzzles for a variety 
 
 <br/>
 
-| Mon  | Tue  | Wed  | Thu  | Fri  | Sat  | Sun  |
-|------|------|------|------|------|------|------|
-|      |      |      |      | ✅✅   | ✅✅ | ✅✅ |
-| ✅✅ | ✅✅ | ✅✅ | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 |
+| Mon   | Tue   | Wed   | Thu   | Fri   | Sat   | Sun   |
+|-------|-------|-------|-------|-------|-------|-------|
+|       |       |       |       | ✅✅ | ✅✅ | ✅✅ |
+| ✅✅ | ✅✅ | ✅✅ | ✅✅ | 🔲🔲 | 🔲🔲 | 🔲🔲 |
 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 |
 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 | 🔲🔲 |
-| 🔲🔲 | 🔲🔲 |      |      |      |      |      |
+| 🔲🔲 | 🔲🔲 |       |       |       |       |       |
 
 
 ```
@@ -36,8 +36,8 @@ Advent of Code is an Advent calendar of small programming puzzles for a variety 
 10|    
 9 |    
 8 |    
-7 |    
-6 |                            ' ~ .'*. ~  ..  ''.
+7 |           .. ~ ~ * ~        . .'''''''''...
+6 |             '''.........' '' ~ .'*. ~  ..  ''.
   |                        .' ~    '...' ~'  '.~  '.
 5 |                        :         ~     '. *'.~ :
   |                 ...'''''          .''.~  '..' .'
---
+++ day-7/puzzle-2.ts
@@ -0,0 +1,104 @@
+// Day 7: Camel Cards
+import * as fs from 'fs';
+
+class Hand {
+    constructor(
+        public cards : string, 
+        public bid : number,
+        public power : number = 0
+        ) {
+            this.setPower()
+    }
+
+    setPower() : void {
+        let jokerCount = 0
+        let cardGroupObj : {[index: string]: number} = [...this.cards].reduce((acc, curr) => {
+            if (curr == 'J') jokerCount++ 
+            else acc[curr] ? acc[curr]++ : acc[curr] = 1 
+            return acc
+        }, {} as {[index: string]: number})
+
+        let cardGroup : number[]
+        if (jokerCount == 5) {
+            cardGroup = [5]
+        } else {
+            cardGroup = Object.values(cardGroupObj).sort()
+            cardGroup[cardGroup.length-1] = cardGroup[cardGroup.length-1]+jokerCount
+        }
+        let cardGroupMax : number = Math.max(...cardGroup)
+
+        if (cardGroup.includes(2) && cardGroup.includes(3)) {
+            this.power = 5
+        }
+        else if (cardGroup.includes(2) && Object.values(cardGroupObj).length == 3) {
+            this.power = 3
+        }
+        else if (cardGroupMax > 3) {
+            this.power = cardGroupMax+2
+        }
+        else if (cardGroupMax == 3) {
+            this.power = cardGroupMax+1
+        } else {
+            this.power = cardGroupMax
+        }
+    }
+}
+
+function cardValue(card : string){
+    switch(card) {
+        case 'A':
+            return 14
+        case 'K':
+            return 13
+        case 'Q':
+            return 12
+        case 'J':
+            return 0
+        case 'T':
+            return 10
+        default:
+            return Number(card)
+    }
+}
+
+function compareCards(a : Hand, b : Hand) : number {
+    let result : number = 0
+    let i : number = 0
+    do {
+        result = cardValue(a.cards[i]) - cardValue(b.cards[i])
+        i++
+    } while (result == 0 && i < 5 )
+    return result
+}
+
+
+function main() {
+    const input : string = fs.readFileSync('day-7/input', 'utf-8')
+    const lines  : string[] = input.split(/\r?\n/)
+    let hands : Hand[] = []
+
+    for (const line of lines) {
+        const [card, bid] = line.split(' ')
+        const hand = new Hand(card, Number(bid))
+        hands.push(hand)
+    }
+
+    let total = 0
+    let cardsArray = hands.sort((a,b) => {
+        if (a.power == b.power) {
+            return compareCards(a,b)
+        } else {
+            return a.power - b.power
+        }
+    })
+    for (let i : number = 0; i < cardsArray.length; i++) {
+        console.log(i, cardsArray[i])
+        total += cardsArray[i].bid * (i+1)
+    }
+    console.log(total)
+}
+
+main()
+
+// let hand : Hand = new Hand('TT221', 1)
+// console.log(hand.power)
