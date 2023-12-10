4715caaf644cb099a04ec14c08c0ecc6ef74376b
Jack Draper <drapj002@gmail.com>
2023-12-10 19:07:35 +0000

    Day 8: Haunted Wasteland

---
+++ day-9/puzzle-1.ts
@@ -0,0 +1,39 @@
+// Day 9: Mirage Maintenance
+import * as fs from 'fs'
+
+function calculateNewElement(history: number[][]) {
+    let addNumber = 0
+    while (history.length != 0) {
+        const current  = history[history.length - 1]
+        addNumber = current[current.length - 1] + addNumber
+        history.pop()
+    }
+    return addNumber
+}
+
+function calculateLists(history: number[][]){
+    if (history[history.length-1].every((val, i, arr) => val === arr[0])){
+        return history
+    }
+    const last : number[]  = history[history.length-1]
+    let newDiff : number[]  = []
+    last.reduce((a, b) => {
+        newDiff.push(b-a)
+        return b})
+
+    return calculateLists([...history, newDiff])
+}
+
+
+function main() {
+    const input : string = fs.readFileSync('./day-9/input', 'utf8')
+    const lines : string[] = input.split(/\r?\n/)
+    let total = 0
+    for (const line of lines) {
+        let numbers : number[] = line.split(' ').map(Number)
+        total += calculateNewElement(calculateLists([numbers]))
+    }
+    console.log(total)
+}
+
+main()
